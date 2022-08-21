5ce2c1ea2cf7355fa4de53a489be1fa59efc58a6
andreystepan <92333697+andreystepan@users.noreply.github.com>
2022-08-21 17:17:46 +0300

    correction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,53 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import API from 'API/API';
+import { BtnLoadMore } from 'components/Button';
 import { Searchbar } from './Searchbar';
 import { ImageGallery } from './ImageGallery';
+import { Modal } from './Modal';
 import { Loader } from './Loader';
 import { useState } from 'react';
+import { useEffect } from 'react';
+
+const imgMapper = dataHits => {
+  return dataHits.map(({ id, webformatURL, largeImageURL }) => {
+    return { id, webformatURL, largeImageURL };
+  });
+};
 
 export function App() {
   const [query, setQuery] = useState('');
   const [loading, setLoading] = useState(false);
+  const [largeImg, setLargeImg] = useState(null);
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    setPage(1);
+    setImages([]);
+  }, [query]);
+
+  useEffect(() => {
+    if (query === '') {
+      return;
+    }
+    setLoading(true);
+    API.fetchApi(query, page)
+      .then(data => setImages(prevImg => [...prevImg, ...imgMapper(data.hits)]))
+      .finally(() => setLoading(false));
+  }, [query, page]);
+
+  const handleClickImg = largeImg => {
+    setLargeImg(largeImg);
+  };
+
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const closeModal = () => {
+    setLargeImg(null);
+  };
 
   const handleFormSubmit = query => {
     setQuery(query);
@@ -18,7 +58,11 @@ export function App() {
       {loading && <Loader />}
       <Searchbar onSubmit={handleFormSubmit} />
       <ToastContainer />
-      <ImageGallery query={query} />
+      <ImageGallery query={query} images={images} onClick={handleClickImg} />
+      {images.length > 0 && <BtnLoadMore onClick={loadMore} />}
+      {largeImg && (
+        <Modal largeImg={largeImg} query={query} onClose={closeModal} />
+      )}
     </div>
   );
 }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,59 +1,16 @@
 import { ImageGalleryItem } from 'components/ImageGalleryItem';
+import PropTypes from 'prop-types';
 
-import API from '../../API/API';
-import { BtnLoadMore } from 'components/Button';
-import { Modal } from '../Modal';
 import { Gallery } from './ImageGallery.styled';
-import { useState } from 'react';
-import { useEffect } from 'react';
 
-const imgMapper = dataHits => {
-  return dataHits.map(({ id, webformatURL, largeImageURL }) => {
-    return { id, webformatURL, largeImageURL };
-  });
-};
-
-export function ImageGallery({ query }) {
-  const [largeImg, setLargeImg] = useState(null);
-  const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setPage(1);
-    setImages([]);
-    setLoading(true);
-  }, [query]);
-
-  useEffect(() => {
-    if (query === '') {
-      return;
-    }
-
-    API.fetchApi(query, page)
-      .then(data => setImages(prevImg => [...prevImg, ...imgMapper(data.hits)]))
-      .finally(() => setLoading(false));
-  }, [query, page]);
-
-  const handleClickImg = largeImg => {
-    setLargeImg(largeImg);
-  };
-
-  const loadMore = () => {
-    setPage(prevPage => prevPage + 1);
-  };
-
-  const closeModal = () => {
-    setLargeImg(null);
-  };
-
+export const ImageGallery = ({ query, images, onClick }) => {
   return (
     <>
       {images.length > 0 && (
         <Gallery>
           {images.map(({ webformatURL, largeImageURL, id }) => (
             <ImageGalleryItem
-              onClick={handleClickImg}
+              onClick={onClick}
               query={query}
               key={id}
               smallImg={webformatURL}
@@ -62,11 +19,18 @@ export function ImageGallery({ query }) {
           ))}
         </Gallery>
       )}
-
-      {images.length > 0 && <BtnLoadMore onClick={loadMore} />}
-      {largeImg && (
-        <Modal largeImg={largeImg} query={query} onClose={closeModal} />
-      )}
     </>
   );
-}
+};
+
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      largeImageURL: PropTypes.string,
+      webformatURL: PropTypes.string,
+    })
+  ),
+  onClick: PropTypes.func,
+  query: PropTypes.string,
+};
